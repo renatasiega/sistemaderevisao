3949c95cd0a880b8b3c80705abc066f1af81ecba
renatasiega <renatasiega@gmail.com>
2016-02-20 22:09:03 -0200

    Cadastro-cliente html ready

--- app/js/config/routeConfig.js
+++ app/js/config/routeConfig.js
@@ -1,4 +1,14 @@
 angular.module("sistemDeRevisao").config(function($routeProvider){
+
+  $routeProvider.when("/", {
+    templateUrl: "view/main.html",
+  });
+
+  $routeProvider.when("/clientes", {
+    templateUrl: "view/clientes.html",
+    controller: "clientesCtrl"
+  });
+
    $routeProvider.when("/cadastro-cliente", {
      templateUrl: "view/cadastro-cliente.html",
      controller: "cadastroClienteCtrl"
